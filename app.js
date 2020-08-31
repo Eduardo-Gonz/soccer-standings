470a6999c87daf72a073382b8da779ce5b544973
Eduardo-Gonz <erugonz249@gmail.com>
2020-08-30 23:01:17 -0700

    Redesigned the section where the standings are located at and added an index.js file that will handle the api fetch requests

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express ();
 
+let footballData = "data";
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
---
+++ public/index.js
@@ -0,0 +1,16 @@
+// fetch("https://api-football-v1.p.rapidapi.com/v2/leagueTable/524", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+// 		"x-rapidapi-key": "5d57b8c6f1mshfea26e104664317p170162jsn99bed2e8e7c0"
+// 	}
+// })
+// .then(response => response.json())
+// .then(data => {
+//   let name = data.api.standings[0][0].teamName;
+//   document.querySelector("#first").innerHTML = name;
+// })
+//
+// .catch(err => {
+// 	console.log(err);
+// });
